fd5623191739e6b80838a90df50ac48a0d2d94d2
Devan Grose <devangrose@gmail.com>
2018-09-07 14:13:34 -0700

    finished lab

--- src/App.js
+++ src/App.js
@@ -10,17 +10,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <Router>
           <div>
             <nav>
-              <Link to="/">Home</Link>
-              <Link to="/projects">Projects</Link>
-              <Link to="/contactme">Contact Me</Link>
-              <Link to="/hjasgd">Stuff</Link>
+              <Link to="/">Home</Link>{' '}
+              <Link to="/projects">Projects</Link>{' '}
+              <Link to="/contactme">Contact Me</Link>{' '}
+              <Link to="/hjasgd">Stuff</Link>{' '}
             </nav>
             <Route exact path="/" component={Home} />
             <Route path="/projects" component={Projects} />
--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
 import HomeContent from './HomeContent';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaPaperPlane } from 'react-icons/fa';
+import { FaFacebookSquare } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { FaPaperclip } from 'react-icons/fa';
+
 
 class Home extends Component {
   componentDidMount(){
     document.title = 'Home';
   }
   render(){
-    return (<HomeContent name="YOURNAMEHERE" />);
+    return (
+      <div>
+        <ContactDetails />
+        <HomeContent name="Devan Grose" /> 
+        <HomeImage source="http://devangrose.me/img/profile.jpg" caption="Look at my beautiful face" />
+        <HomePitch />
+      </div>
+    );
   }
+}
+function HomeImage (props) {
+
+  return <div>
+      <img src={props.source} />
+      <p>{props.caption}</p>
+  </div>
+}
+
+function HomePitch (props) {
+
+  return <div>
+      <h3>Why should you hire me?</h3>
+      <p>I'm just an amazing dude yall</p>
+  </div>
+}
+
+function ContactDetails (props) {
+
+  return <ul>
+    <li><a href="mailto:devangrose@gmail.com"><FaPaperPlane/></a></li>
+    <li><a href="devangrose.me"><FaPaperclip /></a></li>
+    <li><a href="www.linkedin.com/in/devangrose"><FaLinkedin /></a></li>
+    <li><a href="www.github.com/devangrose"><FaGithub /></a></li>
+  </ul>
 }
 
 export default Home;
